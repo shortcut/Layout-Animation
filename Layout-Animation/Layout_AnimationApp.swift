f18d3d3394999b432bb96a53b099a85880fb822a
Vikram <vikram.sh86@gmail.com>
2024-02-05 13:53:37 +0100

    Initial Commit

---
+++ Layout-Animation/Layout_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Layout_AnimationApp.swift
+//  Layout-Animation
+//
+//  Created by Vikram on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Layout_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
